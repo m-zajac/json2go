3c5f8e4f04197eed6cfaae2cf4e9d41a8a12e8f7
Maciej Zając <maz@2021.ai>
2020-05-05 20:09:50 +0200

    WIP

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/m-zajac/json2go
 
 go 1.12
+
+require (
+	github.com/stretchr/testify v1.5.1
+	gopkg.in/yaml.v2 v2.2.8
+)
--- parser_test.go
+++ parser_test.go
@@ -3,8 +3,16 @@ package json2go
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 func ExampleNewJSONParser() {
@@ -346,4 +354,77 @@ type %s struct {
 			}
 		})
 	}
+}
+
+// TestParser tests all cases from files in test/parser directory.
+func TestParser(t *testing.T) {
+	testfilesDir := "test/parser/"
+	err := filepath.Walk(testfilesDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		ext := filepath.Ext(path)
+		if ext != ".json" {
+			return nil
+		}
+		outFile := strings.TrimSuffix(path, ext) + ".out.yaml"
+		baseName := strings.TrimSuffix(path, ext)
+		baseName = strings.TrimPrefix(baseName, testfilesDir)
+
+		testFile(t, baseName, path, outFile)
+
+		return nil
+	})
+	t.Log(err)
+}
+
+func testFile(t *testing.T, name, inPath, outPath string) {
+	t.Helper()
+
+	type testDef struct {
+		Options struct {
+			ExtractCommonTypes           bool `yaml:"extractCommonTypes"`
+			StringPointersWhenKeyMissing bool `yaml:"stringPointersWhenKeyMissing"`
+		} `yaml:"options"`
+		Out string `yaml:"out"`
+	}
+
+	input, err := ioutil.ReadFile(inPath)
+	require.NoError(t, err)
+
+	output, err := ioutil.ReadFile(outPath)
+	require.NoError(t, err)
+
+	var tests []testDef
+	err = yaml.Unmarshal(output, &tests)
+	require.NoError(t, err)
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("%s-%d", name, i), func(t *testing.T) {
+			parserOpts := []JSONParserOpt{
+				OptExtractCommonTypes(tc.Options.ExtractCommonTypes),
+				OptStringPointersWhenKeyMissing(tc.Options.StringPointersWhenKeyMissing),
+			}
+			parser := NewJSONParser(baseTypeName, parserOpts...)
+			err = parser.FeedBytes(input)
+			require.NoError(t, err)
+
+			got := normalizeStr(parser.String())
+			want := normalizeStr(tc.Out)
+
+			assert.Equal(t, want, got)
+		})
+	}
+}
+
+// normalizeStr trims string, replaces all tabs and space groups with single space, collapses multiple new lines into one.
+func normalizeStr(v string) string {
+	v = strings.TrimSpace(v)
+	v = regexp.MustCompile(`[^\S\r\n]+`).ReplaceAllString(v, " ")
+	v = regexp.MustCompile(`[\r\n]+`).ReplaceAllString(v, "\n")
+
+	return v
 }
